94547b67d90fe28e73b5bd4bca5406a571f1de3c
Rustypredator <contact@rusty.info>
2025-09-23 17:12:09 +0200

    deploy and test scripts

---
+++ app/deployment-scripts/laravel-init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd /app
+
+echo "================================================"
+
+echo "          Creating .env file"
+rm .env
+cp .env.example .env
+echo "==============================================="
+
+if [ -f artisan ]; then
+    echo "        Running Laravel Setup Commands"
+    php artisan down
+    php artisan key:generate
+    php artisan migrate --force
+    php artisan scout:flush "App\Models\Location"
+    php artisan scout:flush "App\Models\Entry"
+    php artisan scout:flush "App\Models\Distance"
+
+    php artisan scout:index "App\Models\Location"
+    php artisan scout:index "App\Models\Entry"
+    php artisan scout:index "App\Models\Distance"
+
+    php artisan scout:import "App\Models\Location"
+    php artisan scout:import "App\Models\Entry"
+    php artisan scout:import "App\Models\Distance"
+    
+    echo "==============================================="
+fi
---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "================================================"
+echo "          Removing old stuff"
+sudo docker compose down
+sudo docker container rm explorationdatabase-queueworker-1
+sudo docker container rm explorationdatabase-app-1
+sudo docker container rm explorationdatabase-redis-1
+sudo docker container rm explorationdatabase-db-1
+sudo docker container rm explorationdatabase-search-1
+sudo docker volume rm explorationdatabase_db_data
+sudo docker volume rm explorationdatabase_persistent_data
+sudo docker volume rm explorationdatabase_search_data
+sudo docker image prune -f
+sudo docker volume prune -f
+sudo docker container prune -f
+sudo docker builder prune -f
+echo "==============================================="
+echo "          Starting containers"
+sudo docker compose up  
