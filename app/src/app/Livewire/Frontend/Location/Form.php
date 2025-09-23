45830c0c134b84c859b6e0e4e626dfa159762377
Rustypredator <contact@rusty.info>
2025-09-23 17:55:51 +0200

    entering coords too

--- app/src/app/Livewire/Frontend/Location/Form.php
+++ app/src/app/Livewire/Frontend/Location/Form.php
@@ -13,6 +13,9 @@ class Form extends Component
     public $parent_id;
     public $type;
     public $name;
+    public $x;
+    public $y;
+    public $z;
     public $description;
 
     public function mount($location = null, $parentId = null)
@@ -29,6 +32,9 @@ class Form extends Component
             $this->parent_id = $this->location->parent_id;
             $this->type = $this->location->type;
             $this->name = $this->location->name;
+            $this->x = $this->location->x;
+            $this->y = $this->location->y;
+            $this->z = $this->location->z;
             $this->description = $this->location->description;
         }
         // fetch possible parent locations:
@@ -49,6 +55,9 @@ class Form extends Component
         $this->location->parent_id = $this->parent_id;
         $this->location->type = $this->type;
         $this->location->name = $this->name;
+        $this->location->x = $this->x;
+        $this->location->y = $this->y;
+        $this->location->z = $this->z;
         $this->location->description = $this->description;
         // Save everything
         if ($this->location->save()) {
